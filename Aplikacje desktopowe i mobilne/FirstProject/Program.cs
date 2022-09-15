b31b0be072b33c5490642b69b210243a477fe82c
kacpergoluchowski <kgoluchowski112@gmail.com>
2022-09-15 09:28:05 +0200

    koniec zajec 15.09.2022

--- Aplikacje desktopowe i mobilne/FirstProject/Program.cs
+++ Aplikacje desktopowe i mobilne/FirstProject/Program.cs
@@ -7,6 +7,21 @@ namespace FirstProject
         static void Main(string[] args)
         {   
             Console.WriteLine("Hello World!");
+
+            string imie = "Jan";
+            string nazwisko = "Kowalski";
+            // x = nazwisko.Length;
+            // Console.WriteLine(x);
+            Console.WriteLine("Witaj " + imie + " " + nazwisko);
+            Console.WriteLine("Witaj {0} {1} tutaj", imie, nazwisko);
+            Console.WriteLine($"Witaj {imie} {nazwisko} tutaj", imie, nazwisko);
+            Console.WriteLine(@"Prędkość to km\h");
+
+            Console.WriteLine(float.MaxValue);
+            double a = 5.6;
+            Console.WriteLine(a);
+
+            int b = 6. ;
         }
     }
 }
